21c2cc6eeffa14d40ddfb41867e1489cb6bfbb77
woon <woon7713@gmail.com>
2025-05-30 14:41:59 +0900

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,48 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.*; // ArrayList 등 import
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        byte level = 1;
+        short cm = 180;
+        int age = 20;
+        long m = 200;
+        float pi = 1.123456789f;     // float: 정밀도 약 7자리
+        double k = 1.1234567890123456; // double: 정밀도 약 15~16자리
+        /*
+        float은 가볍지만 부정확, double은 무겁지만 더 정확
+        float 4 byte | 단정도 32bit 정밀도
+        dobule 8 byte | 배정도 64bit 정밀도
+         */
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        char grade = 'A';
+        boolean real = true;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String name = "James"; // 문자(char)는 ' ' || 문자열(String)은 " " -> 언어마다 다름
+        int[] box = {1, 2, 3};
+
+        System.out.println("age = " + age + ", " + level + "년 후");
+        age = 21;
+        System.out.println("age = " + age);
+
+        Dog dog = new Dog();
+        System.out.println("개 종류" + dog.category);
+
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+
+        for(String fruit: fruits){
+            System.out.println(fruit);
         }
+        System.out.println(fruits.get(1));// List의 특정 요소에 접근
+
+
     }
+}
+
+class Dog{
+    String category = "진돗개";
+    boolean sex = false; // false = 암컷, true = 수컷
+
+
 }
