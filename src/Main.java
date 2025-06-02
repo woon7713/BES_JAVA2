7a32f809bf3716fc3b03dadf6e01925a5feed08d
woon <woon7713@gmail.com>
2025-06-02 11:24:43 +0900

    전치연산 후위연산

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,21 @@ public class Main {
     public static void main(String[] args) {
         int a = 10, b = 3;
 
-        a += 5; // 복합대입 연산자, a = a + 5;
-
+        ++a;
         System.out.println(a);
 
-        b -= 1;
-        System.out.println(b);
-        b *= 3;
-        System.out.println(b);
-        b /= 2;
-        System.out.println(b);
-        b %= 2;
+        --b;
         System.out.println(b);
 
+        a++;
+        System.out.println(a);
+
+        b--;
+        System.out.println(b);
+
+        // 후위 연산 직관적 확인
+        int c = a++; // 후위연산, 할당 후 증가
+        System.out.println(c + ", " + a);
 
     }
 }
