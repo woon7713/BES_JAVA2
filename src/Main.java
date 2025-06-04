ae5a2cb7e4355ffb36238294796197bad1db5cb3
woon <woon7713@gmail.com>
2025-06-04 15:12:27 +0900

    do while

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        boolean a = false;
+        Scanner in = new Scanner(System.in); // Scanner 객체 생성
+        int input;
 
         do {
-            System.out.println("무한 루프?");
-        } while (a);
+            System.out.println("자판기입니다.");
+            input = in.nextInt();
 
-        int i = 0;
-        a = true;
-
-        while (a) {
-            System.out.println(i);
-            i++;
-
-            if(i>10000){
-                a= false;
+            switch (input) {
+                case 1:
+                    System.out.println("coke");
+                    break;
+                case 2:
+                    System.out.println("cider");
+                    break;
+                case 3:
+                    System.out.println("3 입력");
+                    break;
+                default:
+                    System.out.println("잘못된 입력");
             }
 
-        }
+        } while (input != 0);
+
 
     }
 }
