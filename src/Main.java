a0fa9ed6e1fc5228f1b8dcb394b5440cca19c07b
woon <woon7713@gmail.com>
2025-06-05 15:04:10 +0900

    function2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 public class Main {
+    public static int addNumber(int a, int b) {
+        int result = a + b;
 
-    public static void printStudent(String name, int age){
-        System.out.println("이름: " + name);
-        System.out.println("나이: " + age);
-        System.out.println("-------------");
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println("=== 학생 정보 출력 ===");
-        printStudent("James", 20);
-        printStudent("Stax", 32);
-        printStudent("ABC", 45);
+        int number = addNumber(10, 7);
 
+        System.out.println(number);
     }
 }
