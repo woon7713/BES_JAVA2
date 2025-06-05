7867fb6f59ffab836a7dea21b13f3358af3582b7
woon <woon7713@gmail.com>
2025-06-05 14:20:34 +0900

    function

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,16 @@
 public class Main {
+
+    public static void printStudent(String name, int age){
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("-------------");
+    }
+
     public static void main(String[] args) {
-//        int a = 3;
-//        int b = 4;
-//        int max = (a>b) ? a:b;
-//        System.out.println(max);
-//
-//        if(a>b){
-//            max = a;
-//        }
-//        else{max = b;}
-//
-//        System.out.println(max);
-//
-//
-//        int score = 60;
-//        String PF = (score >= 60) ? "통과" : "탈락";
-//        System.out.println(PF);
-//
-//        if(score >= 60){
-//            PF = "통과";
-//        }
-//        else{
-//            PF = "탈락";
-//        }
-//        System.out.println(PF);
-
-        String color = "파랑";
-
-        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : "빨강 파랑 외";
-        System.out.println(choiceColor);
-
-        if (color == "빨강") {
-            System.out.println("빨강");
-        } else if (color == "파랑") {
-            System.out.println("파랑");
-        } else {
-            System.out.println("보라");
-        }
-
-
-
+        System.out.println("=== 학생 정보 출력 ===");
+        printStudent("James", 20);
+        printStudent("Stax", 32);
+        printStudent("ABC", 45);
 
     }
 }
