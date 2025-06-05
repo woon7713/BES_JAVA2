bd916f12a0281efcbc4241d44d56f0ce7a07e2e8
woon <woon7713@gmail.com>
2025-06-05 13:06:13 +0900

    ternary operator

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-/*        for (int i = 1; i <= 10; i++) {
-            if (i == 7) {
-                break;
-            }
-            System.out.println(i);
+//        int a = 3;
+//        int b = 4;
+//        int max = (a>b) ? a:b;
+//        System.out.println(max);
+//
+//        if(a>b){
+//            max = a;
+//        }
+//        else{max = b;}
+//
+//        System.out.println(max);
+//
+//
+//        int score = 60;
+//        String PF = (score >= 60) ? "통과" : "탈락";
+//        System.out.println(PF);
+//
+//        if(score >= 60){
+//            PF = "통과";
+//        }
+//        else{
+//            PF = "탈락";
+//        }
+//        System.out.println(PF);
+
+        String color = "파랑";
+
+        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : "빨강 파랑 외";
+        System.out.println(choiceColor);
+
+        if (color == "빨강") {
+            System.out.println("빨강");
+        } else if (color == "파랑") {
+            System.out.println("파랑");
+        } else {
+            System.out.println("보라");
         }
 
-        for (int i = 1; i <= 20; i++) {
-            if (i % 3 != 0) {
-                continue;
-            }
-            System.out.println(i);
-        }*/
 
-        for (int i = 1; i <= 20; i++) {
-            if(i % 5 == 0){
-                continue;
-            }
-            if(i >= 15){
-                break;
-            }
-            System.out.print(i + " ");
-        }
+
 
     }
 }
