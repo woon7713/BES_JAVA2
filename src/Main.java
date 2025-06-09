b43e0bdaecd303fbf3cad1399ea81c6e5087c64d
woon <woon7713@gmail.com>
2025-06-09 13:30:20 +0900

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
-
-    static int add(int a, int b) {
-        return a + b;
-    }
-
-    static int add(int a, int b, int c) {
-        return a + b + c;
-    }
-
-    static double add(double a, double b) {
-        return a + b;
-    }
-
-    static String greet(){
-
-        return "Hello, World!";
-    }
-
-    static String greet(String name) {
-
-        return "Hello, " + name + "!";
-    }
-
-    static String greet(String name, int age) {
-
-        return name + age;
-    }
-
     public static void main(String[] args) {
-//        System.out.println(add(3, 4));
-//        System.out.println(add(3, 4, 7));
-//        System.out.println(add(1.2, 2.3));
 
-        System.out.println(greet());
-        System.out.println(greet("Alice"));
-        System.out.println(greet("Zonic", 3));
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(numbers));
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("numbers [" + i + "] = " + numbers[i]);
+        }
+
+        for (int i = numbers.length-1; i >= 0; i--) {
+            System.out.println("numbers [" + i + "] = " + numbers[i]);
+        }
+
+
+        System.out.println(numbers.length);
+
+
+        int[] box = {2, 4, 6, 8, 10};
+        int sum = 0;
+        for (int i = 0; i < box.length; i++) {
+            sum += box[i];
+        }
+        System.out.println(sum);
+
+
     }
 }
