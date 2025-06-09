fce91d2b146d6308008edd85668a62169d4a6f87
woon <woon7713@gmail.com>
2025-06-09 10:41:04 +0900

    Method Overloading

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
 public class Main {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    static String greet(){
+
+        return "Hello, World!";
+    }
+
+    static String greet(String name) {
+
+        return "Hello, " + name + "!";
+    }
+
+    static String greet(String name, int age) {
+
+        return name + age;
+    }
+
     public static void main(String[] args) {
-        char asciiChar = 'A';
-        System.out.println("Character: " + asciiChar);
+//        System.out.println(add(3, 4));
+//        System.out.println(add(3, 4, 7));
+//        System.out.println(add(1.2, 2.3));
 
-        int asciiCode = (int) asciiChar;
-        System.out.println("ASCII Code: " + asciiCode);
-
-        char koreanChar = '가';
-        System.out.println("Character: " + koreanChar);
-
-        int unicodeCode = (int) koreanChar;
-        System.out.println("Unicode Code: U+" + Integer.toHexString(unicodeCode).toUpperCase());
+        System.out.println(greet());
+        System.out.println(greet("Alice"));
+        System.out.println(greet("Zonic", 3));
     }
 }
