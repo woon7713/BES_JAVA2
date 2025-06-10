280cd1d1a51b535c9c5e4598013132e3dc4c80ba
woon <woon7713@gmail.com>
2025-06-10 15:39:53 +0900

    Constructors

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        Person person = new Person();
-        person.name = "홍길동";
-        person.age = 20;
-        person.cm = 185.0;
-        person.weight = 93.5;
-        person.sayHello();
-        person.work();
-
-        Person mzPerson = new Person();
-        mzPerson.name = "김엠지";
-        mzPerson.age = 18;
-        mzPerson.MBTI = "ESTJ";
-
-        Car car = new Car();
-        car.brand = "newBrand";
-        car.color = "Red";
-        car.year = 2015;
-        car.displayInfo();
-        System.out.println(car.paybackMoney() + "원");
-
-
-
-
-
-
-    }
-}
-
 class Person{
     String name;
     int age;
-    double cm;
-    double weight;
-    String MBTI;
 
-
-    public void sayHello(){
-        System.out.println("이름은 " + name + " 나이는 " + age);
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
-    public void work(){
-        System.out.println("일하는중..");
+    void hi(){
+        System.out.println(name + age);
     }
-
-
-
 }
 
-class Car{
-    String brand;
-    int year;
+public class Main{
+    public static void main(String[] args) {
+        Person person = new Person("Alice", 20); //1
 
-    String color;
+//        person.name = "홍길동"; //2
+//        person.age = 20; //3
 
-    void displayInfo(){
-        System.out.println(brand + " " + year + " " + color);
+        person.hi();//4
+
     }
-
-    int paybackMoney(){
-        return (year - 1990) * 180000;
-    }
-
-
-
 }
