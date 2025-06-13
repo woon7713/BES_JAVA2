d55fa2f2bc8318a473d1257b946b21f2f7cab808
woon <woon7713@gmail.com>
2025-06-13 13:11:43 +0900

    Polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,53 @@
-class Person{
-    public String name;
-    private int age;
-
-    public int getAge(){
-        return age;
-    }
-
-    public void setAge(int age){
-        if(age > 0){
-            this.age = age;
-        }
-        else{
-            System.out.println("그런 나이는 없어요");
-        }
+class Animal{
+    public void makeSound(){
+        System.out.println();
     }
 
 }
 
+class Dog extends Animal{
+
+    @Override
+    public void makeSound(){
+        System.out.println("월월");
+    }
+
+
+}
+
+class Cat extends Animal{
+    @Override
+    public void makeSound(){
+        System.out.println("야옹");
+    }
+
+
+}
+
+class Bird extends Animal{
+    @Override
+    public void makeSound(){
+        System.out.println("짹");
+    }
+
+
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person();
-        person1.name = "홍길동";
-        person1.setAge(1);
+        // 다형성
 
-        System.out.println(person1.name);
-        System.out.println(person1.getAge());
+        Animal dog1 = new Dog();
+        Animal cat1 = new Cat();
+        Animal bird1 = new Bird();
+
+        Animal[] animals = {dog1, cat1, bird1}; // 타입통일로 배열에 같이 넣을 수 있게됨
+        dog1 = cat1; // 부모타입이므로 다른 자식 객체 참조도 할당해서 교체 가능. 하지만 실제 객체 타입은 달라 주의 필요.
+
+        for (Animal animal : animals) {
+            animal.makeSound();
+        }
+
     }
 }
