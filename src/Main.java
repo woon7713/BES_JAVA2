b2300bc0e90bb248d9fae266bba9660e7d45ef56
woon <woon7713@gmail.com>
2025-06-16 10:29:27 +0900

    static

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,31 @@
-class Animal{
-    public void makeSound(){
-        System.out.println();
+class Person{
+    int a = 0;
+    static int b = 1;
+
+    public static void hi(){
+        System.out.println("hi static method");
+        //System.out.println(a); // 접근 X
+        System.out.println(b);
     }
 
-}
-
-class Dog extends Animal{
-
-    @Override
-    public void makeSound(){
-        System.out.println("월월");
-    }
-
-
-}
-
-class Cat extends Animal{
-    @Override
-    public void makeSound(){
-        System.out.println("야옹");
+    public void hello() {
+        System.out.println("hello");
     }
 
 
 }
-
-class Bird extends Animal{
-    @Override
-    public void makeSound(){
-        System.out.println("짹");
-    }
-
-
-}
-
 
 public class Main {
     public static void main(String[] args) {
-        // 다형성
+        //static 메서드는 클래스 이름으로 직접 호출
+        Person.hi();
 
-        Animal dog1 = new Dog();
-        Animal cat1 = new Cat();
-        Animal bird1 = new Bird();
+        //인스턴스 메서드는 객체를 생성해 호출
+        Person p = new Person();
+        p.hello();
 
-        Animal[] animals = {dog1, cat1, bird1}; // 타입통일로 배열에 같이 넣을 수 있게됨
-        dog1 = cat1; // 부모타입이므로 다른 자식 객체 참조도 할당해서 교체 가능. 하지만 실제 객체 타입은 달라 주의 필요.
 
-        for (Animal animal : animals) {
-            animal.makeSound();
-        }
+
 
     }
 }
