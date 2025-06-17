a71593e0d9700f075ffa01bbe6ee322e11953cbb
woon <woon7713@gmail.com>
2025-06-17 10:15:25 +0900

    Lambda Expression

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,22 @@
-class CPU {
-    public int register = 0;
-    public int[] memory = {5, 10, 0};
-
-    private static final int LOAD = 1;
-    private static final int SAVE = 2;
-    private static final int ADD = 3;
-    private static final int SUB = 4;
-    private static final int HALT = 5;
-
-    private int[][] program = {
-            { LOAD, 0 },         // register = memory[0]
-            { ADD, 1 },         // register = register + memory[1]
-            { SAVE, 2 },         // memory[2] = register
-            { HALT, 0 }         // stop program execution
-    };
-
-    public void executeProgram() {
-        int pc = 0;
-
-        while(true) {
-            int opcode = program[pc][0];
-            int operand = program[pc][1];
-
-            switch (opcode) {
-                case LOAD:
-                    register = memory[operand];
-                    System.out.println("LOAD: Loading memory[" + operand + "] (" + memory[operand] + ") into register.");
-                    break;
-                case SAVE:
-                    memory[operand] = register;
-                    System.out.println("SAVE: Saving memory[" + operand + "] (" + memory[operand] + ") from register.");
-                    break;
-                case ADD:
-                    register += memory[operand];
-                    System.out.println("ADD: Adding memory [" + operand + "] (" + memory[operand] +  ") into register.");
-                    break;
-                case SUB:
-                    register -= memory[operand];
-                    System.out.println("SUB: subtracting memory [" + operand + "] )" + memory[operand] +  ") into register.");
-                    break;
-                case HALT:
-                    System.out.println("HALT: Stopping program execution");
-                    return;
-                default:
-                    System.out.println("Unknown instruction. Halting");
-                    return;
-
-            }
-            pc++;
-
-
-        }
-    }
+@FunctionalInterface
+interface MathOperation {
+    int operate(int a, int b);
 }
-
+@FunctionalInterface
+interface Operation {
+    int apply(int x);
+}
 public class Main {
     public static void main(String[] args) {
-        CPU cpu = new CPU();
-        cpu.executeProgram();
-        System.out.println("final memory state: " + cpu.memory[2]);
+        MathOperation add = (a, b) -> a + b;
+        MathOperation multiply = (a, b) -> a * b;
+        System.out.println(add.operate(5, 3));
+        System.out.println(multiply.operate(5, 3));
+
+        Operation square = (x) -> x * x;
+        System.out.println(square.apply(7));
+
+
 
     }
 }
