94a0c3016ffa2cbd2f7bb018cdbed4f08395a380
woon <woon7713@gmail.com>
2025-06-17 14:12:30 +0900

    throw

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 public class Main {
-    public static int convertToInt(String s) throws NumberFormatException {
-        return Integer.parseInt(s);
-    }
     public static void main(String[] args) {
-        String strNum = "1234a";
+        String input = "100";
+
         try {
-            int num = convertToInt(strNum);
-            System.out.println("변환 결과: " + num);
-        } catch (NumberFormatException e) {
-            System.out.println("숫자 형식 오류!");
+            boolean result = validateTokenCount(input);
+
+            if (result) {
+                System.out.println("숫자 2개");
+            }
+        } catch(NumberFormatException e) {
+            System.out.println(e.getMessage());
         }
 
+    }
 
+    public static boolean validateTokenCount(String tokens) throws NumberFormatException {
+        if (tokens.length() != 2) {
+            throw new NumberFormatException("숫자 2개가 아닙니다.");
+        }
 
+        return true;
     }
 }
