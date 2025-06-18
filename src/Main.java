da729515ed4216f8bed14b9ff86de1dd3279387e
woon <woon7713@gmail.com>
2025-06-18 13:27:55 +0900

    LinkedHashSet

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        Set<String> set = new HashSet<>();
+        Set<String> linkedSet = new LinkedHashSet<>();
+        linkedSet.add("Banana");
+        linkedSet.add("Apple");
+        linkedSet.add("Cherry");
+        linkedSet.add("Banana");
 
-        list.add("Apple");
-        list.add("Apple");
-        set.add("Banana");
-        set.add("Banana"); // 중복 추가 무시
-
-        System.out.println(list);
-        System.out.println(set);
+        System.out.println(linkedSet); // [Banana, Apple, Cherry]
 
 
 
