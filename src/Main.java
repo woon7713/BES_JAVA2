ffa556721e7bf7a8958ed7c863c87235edad1d3d
woon <woon7713@gmail.com>
2025-06-18 13:29:41 +0900

    TreeSet

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> linkedSet = new LinkedHashSet<>();
-        linkedSet.add("Banana");
-        linkedSet.add("Apple");
-        linkedSet.add("Cherry");
-        linkedSet.add("Banana");
+        Set<String> treeSet = new TreeSet<>();
+        treeSet.add("Banana");
+        treeSet.add("Apple");
+        treeSet.add("Banana");
+        treeSet.add("Cherry");
 
-        System.out.println(linkedSet); // [Banana, Apple, Cherry]
-
-
+        System.out.println(treeSet); // [Apple, Banana, Cherry]
 
     }
 }
