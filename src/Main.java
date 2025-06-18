9f5941f2048409d615b594c3bdeaded2a71f5d9e
woon <woon7713@gmail.com>
2025-06-18 13:26:44 +0900

    Set

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,20 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> fruits = new ArrayList<>();
-        fruits.add("Mango");
-        fruits.add("Dragonfruit");
-        fruits.add("Cherry");
-        fruits.add("Strawberry");
-        fruits.add("Banana");
-        fruits.add("Tomato");
-        fruits.add("Apple");
+        List<String> list = new ArrayList<>();
+        Set<String> set = new HashSet<>();
 
-        fruits.remove("Tomato");
-        fruits.remove(0);
+        list.add("Apple");
+        list.add("Apple");
+        set.add("Banana");
+        set.add("Banana"); // 중복 추가 무시
 
-        Collections.sort(fruits);
-
-        for (String fruit: fruits) {
-            System.out.println(fruit);
-        }
-
-        // equals로 체크해서 던져주는 내부 로직 때문에 대소문자까지 체크
-        if(fruits.contains("Banana")) {
-            System.out.println("Banana 포함되어 있습니다.");
-        }
-
-        int index = fruits.indexOf("Banana");
-        fruits.set(1, "AppleMango");
-        List<String> sliced = fruits.subList(0, 2);
-
-        for(String slicedList: sliced){
-            System.out.println(slicedList);
-        }
-
-        System.out.println("리스트 크기: " + fruits.size());
-
-        fruits.clear();
-        System.out.println(fruits);
-
-        if (fruits.isEmpty()) {
-            System.out.println("리스트가 비어있음");
-        }
-        else{
-            System.out.println("리스트에 요소가 존재");
-        }
-
-        System.out.println("리스트 크기: " + fruits.size());
-
-
+        System.out.println(list);
+        System.out.println(set);
 
 
 
