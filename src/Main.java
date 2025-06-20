3ea2803cee05d7abc4460f9ddc7cdd16981846f7
woon <woon7713@gmail.com>
2025-06-20 10:47:17 +0900

    Stream average

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,14 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        Stream<Integer> stream = numbers.stream();
-        int result = numbers.stream().filter(n -> n%2 == 0).mapToInt(Integer::intValue).sum();
-        System.out.println(result);
+        List<Double> values = Arrays.asList(10.0, 20.0, 30.0);
 
+        double avg = values.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0.0); // 기본값 설정, 안전장치 . 0으로 나눌수없음, 갯수가 0일땐 평균이 0
+
+        System.out.println(avg);
 
 
 
