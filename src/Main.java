1c70e7ad74c41697827415dbf6e240b8899d7f9e
woon <woon7713@gmail.com>
2025-06-20 16:13:27 +0900

    synchronized

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,40 @@
-class SumRunnable implements Runnable{
-    private final int[] numbers;
+class Counter {
+    private int count = 0;
 
-    public SumRunnable(int[] numbers){
-        this.numbers = numbers;
+    public synchronized void increment() {
+        count++;
     }
 
-    @Override
-    public void run(){
-        int sum = 0;
-        for(int n : numbers){
-            sum+= n;
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println("Thread Interrupted");
-            }
-        }
-        System.out.println(Thread.currentThread().getName() + " - Sum: " + sum);
-
-
+    public int getCount() {
+        return count;
     }
-
 }
 
 public class Main {
     public static void main(String[] args) {
-        int[][] dataSets = {
-                {1, 2, 3, 4, 5},
-                {10, 20, 30},
-                {7, 14, 21, 28},
-                {100, 200, 300, 400}
+
+        Counter counter = new Counter();
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
         };
 
-        for (int i = 0; i < dataSets.length; i++) {
-            Thread thread = new Thread((new SumRunnable(dataSets[i])), "Thread-" + i);
-            thread.start();
-
+        Thread[] threads = new Thread[5];
+        for (int i = 0; i < 5; i++) {
+            threads[i] = new Thread(task);
+            threads[i].start();
         }
+        for (Thread t : threads) {
+            try{
+                t.join(); // 각 스레드를 기다림
 
-        // 결과 : 별도로 돌아가니깐 갯수가 적은 순으로 완료되서 출력(cpu 계산 로직대로) -> 순서를 지정할 수 없음.
-
+            }
+            catch (InterruptedException e){
+                System.out.println("Intterupted");
+            }
+        }
+        System.out.println("Final count: " + counter.getCount());
 
 
 
