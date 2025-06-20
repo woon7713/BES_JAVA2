7ba46d7e86a7dcd905e29c38cc572e6601868ace
woon <woon7713@gmail.com>
2025-06-20 09:39:18 +0900

    Stream even sum

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,16 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
-    // 확장이 Number 까지만
-    public static double sumList(List<? extends Number> list) {
-        double sum = 0;
-
-        for (Number n : list) {
-            sum += n.doubleValue();
-        }
-        return sum;
-    }
-
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(10, 20, 30);
-        List<Double> doubleList = Arrays.asList(0.3, 0.2);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        Stream<Integer> stream = numbers.stream();
+        int result = numbers.stream().filter(n -> n%2 == 0).mapToInt(Integer::intValue).sum();
+        System.out.println(result);
 
-        System.out.println("합계: " + sumList(intList));
-        System.out.println("합계: " + sumList(doubleList));
+
 
 
     }
