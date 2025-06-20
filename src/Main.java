9677324749cadcd0fa9851421d0e20cc9bf2d927
woon <woon7713@gmail.com>
2025-06-20 14:34:10 +0900

    Runnable SumRunnable

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,47 @@
+class SumRunnable implements Runnable{
+    private final int[] numbers;
+
+    public SumRunnable(int[] numbers){
+        this.numbers = numbers;
+    }
+
+    @Override
+    public void run(){
+        int sum = 0;
+        for(int n : numbers){
+            sum+= n;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread Interrupted");
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " - Sum: " + sum);
+
+
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        new Thread(() -> {
-            System.out.println("Runnable 실행 중");
+        int[][] dataSets = {
+                {1, 2, 3, 4, 5},
+                {10, 20, 30},
+                {7, 14, 21, 28},
+                {100, 200, 300, 400}
+        };
 
-            for(int i = 1; i <= 5; i++) {
-                try {
-                    Thread.sleep(5000);
+        for (int i = 0; i < dataSets.length; i++) {
+            Thread thread = new Thread((new SumRunnable(dataSets[i])), "Thread-" + i);
+            thread.start();
 
-                    System.out.println(i);
-                } catch(InterruptedException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }).start();
+        }
+
+        // 결과 : 별도로 돌아가니깐 갯수가 적은 순으로 완료되서 출력(cpu 계산 로직대로) -> 순서를 지정할 수 없음.
+
+
+
+
     }
 }
