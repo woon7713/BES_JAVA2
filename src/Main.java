8389defbd951c7aac1d584d43a691acc90acbeed
woon <woon7713@gmail.com>
2025-06-24 14:35:26 +0900

    NIO

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,53 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class Main {
     public static void writeFile(String filename, String content) {
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter(filename, true);
-            writer.write(content);
+        Path filePath = Paths.get(filename);
+
+        try (FileChannel writeChannel = FileChannel.open(filePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            buffer.put(content.getBytes());
+            buffer.flip();
+            writeChannel.write(buffer);
+            System.out.println("파일을 성공적으로 작성 완료했습니다.");
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (writer != null) writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
-
     }
 
-
     public static void readFile(String filename) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+        Path filePath = Paths.get(filename);
+
+        try (FileChannel readChannel = FileChannel.open(filePath, StandardOpenOption.READ)) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            int bytesRead = readChannel.read(buffer);
+
+            while (bytesRead != -1) {
+                buffer.flip();
+
+                String chunk = StandardCharsets.UTF_8.decode(buffer).toString();
+                System.out.print(chunk);
+
+                buffer.clear();
+                bytesRead = readChannel.read(buffer);
             }
+            System.out.println("\n파일 읽기가 완료 되었습니다.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-//        writeFile("example.txt", "Hello, Java I/O\nthis is a Test2.");
-//        System.out.println("파일 생성 완료");
-        readFile("example.txt");
-
+        writeFile("dinner-menu.txt", "볶음밥\n삼겹살\n냉면");
+        readFile("dinner-menu.txt");
     }
 }
